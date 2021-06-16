b4c43a4e4e5e4cb3820f6e20e0fabadfbb0b3cb7
Muukii <muukii.app@gmail.com>
2021-06-16 15:56:59 +0900

    Update Demo

--- Demo_MondrianLayout/ExampleView.swift
+++ Demo_MondrianLayout/ExampleView.swift
@@ -12,14 +12,22 @@ final class ExampleView: UIView {
     super.init(frame: .zero)
     build(self)
 
-    setContentHuggingPriority(.required, for: .horizontal)
-    setContentHuggingPriority(.required, for: .vertical)
+//    setContentHuggingPriority(.required, for: .horizontal)
+//    setContentHuggingPriority(.required, for: .vertical)
 
     translatesAutoresizingMaskIntoConstraints = false
 
 //    buildSelfSizing {
 //      $0.height(height)
 //    }
+
+    let fittingWdith = widthAnchor.constraint(equalToConstant: 0)
+    fittingWdith.priority = .fittingSizeLevel
+    fittingWdith.isActive = true
+
+    let fittingHeight = heightAnchor.constraint(equalToConstant: 0)
+    fittingHeight.priority = .fittingSizeLevel
+    fittingHeight.isActive = true
 
     if let width = width {
       widthAnchor.constraint(equalToConstant: width).isActive = true
