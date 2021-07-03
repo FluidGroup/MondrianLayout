46bce3567c86b3fb8cc0a42f6e424beec871a702
Muukii <muukii.app@gmail.com>
2021-07-03 14:45:42 +0900

    API changes in classic layout (#25)
    * Rename
    
    * Patch
    
    * :evergreen_tree: Update
    
    * Update readme

--- Demo_MondrianLayout/Book.Classic.swift
+++ Demo_MondrianLayout/Book.Classic.swift
@@ -11,46 +11,65 @@ var _book_classic: BookView {
         let box1 = UIView.mock(backgroundColor: .mondrianBlue, preferredSize: .smallSquare)
         let box2 = UIView.mock(backgroundColor: .mondrianBlue, preferredSize: .smallSquare)
 
-        let containerCenterDemo = UIView.mock(backgroundColor: .mondrianGray, preferredSize: .largeSquare)
-        let containeeCenterDemo = UIView.mock(backgroundColor: .mondrianBlue, preferredSize: .smallSquare)
-
-        let containerEdgesDemo = UIView.mock(backgroundColor: .mondrianYellow, preferredSize: .largeSquare)
-        let containeeEdgesDemo = UIView.mock(backgroundColor: .mondrianBlue)
-
         view.addSubview(box1)
         view.addSubview(box2)
-        view.addSubview(containerCenterDemo)
-        view.addSubview(containerEdgesDemo)
-
-        containerCenterDemo.addSubview(containeeCenterDemo)
-        containerEdgesDemo.addSubview(containeeEdgesDemo)
-
 
         mondrianBatchLayout {
 
           box1.mondrian.layout
-            .topToSuperview()
-            .leftToSuperview()
-            .right(to: box2, .left)
-            .bottom(to: box2, .bottom)
+            .top(.toSuperview)
+            .left(.toSuperview)
+            .right(.to(box2), .left)
+            .bottom(.to(box2), .bottom)
 
           box2.mondrian.layout
-            .topToSuperview(.top, .constant(10))
-            .rightToSuperview()
+            .top(.toSuperview, .top, .constant(10))
+            .right(.toSuperview)
+            .bottom(.toSuperview)
+
+        }
+      }
+    }
+
+    BookPreview {
+      ExampleView(width: nil, height: nil) { view in
+
+        let containerCenterDemo = UIView.mock(backgroundColor: .mondrianGray, preferredSize: .largeSquare)
+        let containeeCenterDemo = UIView.mock(backgroundColor: .mondrianBlue, preferredSize: .smallSquare)
+
+        view.addSubview(containerCenterDemo)
+
+        containerCenterDemo.addSubview(containeeCenterDemo)
+
+        mondrianBatchLayout {
 
           containerCenterDemo.mondrian.layout
-            .top(to: box2, .bottom, .constant(20))
-            .bottomToSuperview()
+            .edges(.toSuperview)
 
           containeeCenterDemo.mondrian.layout
-            .centerToSuperView()
+            .center(.toSuperview)
+
+        }
+      }
+    }
+
+    BookPreview {
+      ExampleView(width: nil, height: nil) { view in
+
+        let containerEdgesDemo = UIView.mock(backgroundColor: .mondrianYellow, preferredSize: .largeSquare)
+        let containeeEdgesDemo = UIView.mock(backgroundColor: .mondrianBlue)
+
+        view.addSubview(containerEdgesDemo)
+
+        containerEdgesDemo.addSubview(containeeEdgesDemo)
+
+        mondrianBatchLayout {
 
           containerEdgesDemo.mondrian.layout
-            .top(to: containerCenterDemo, .top, .constant(0))
-            .leading(to: containerCenterDemo, .trailing, .constant(5))
+            .edges(.toSuperview)
 
           containeeEdgesDemo.mondrian.layout
-            .edgesToSuperView(.constant(8))
+            .edges(.toSuperview, .constant(8))
         }
       }
     }
