1d068a06676cfe4acce04d1960b711422f109f2e
Muukii <muukii.app@gmail.com>
2021-11-25 01:26:48 +0900

    Add docs

--- MondrianLayout/Manager/LayoutManager.swift
+++ MondrianLayout/Manager/LayoutManager.swift
@@ -1,16 +1,26 @@
 
 import UIKit
 
+/**
+ An object that manages layout.
+ It supports updating the layout.
+ In the case of defining distinct layouts under some conditions, this helps.
+ */
 public final class LayoutManager {
 
   private var _layoutBuilder: (() -> [EntrypointBuilder.Either])?
   private var currentContext: LayoutBuilderContext?
   private weak var targetView: UIView?
 
-  public init(initialContext: LayoutBuilderContext? = nil) {
-    self.currentContext = initialContext
+  public init() {
   }
 
+  /**
+   Setting up the layout of subviews on the view.
+   the layout closure is called each calling `reloadLayout` and after `setup`.
+
+   Please avoid creating view and layout-guide instances in the closure. Performance decreases by creating a new instance and destroying the previous one.
+   */
   public func setup(
     on view: UIView,
     @EntrypointBuilder layout: @escaping () -> [EntrypointBuilder.Either]
@@ -20,6 +30,9 @@ public final class LayoutManager {
     reloadLayout()
   }
 
+  /**
+   Re lays out the subviews with deactivating the current layout.
+   */
   public func reloadLayout() {
     guard let _layoutBuilder = _layoutBuilder else {
       return
