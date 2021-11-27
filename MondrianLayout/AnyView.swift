3558abffd2376b0d24dc78b61798b8c8ab94ce2d
Muukii <muukii.app@gmail.com>
2021-11-28 01:53:43 +0900

    Update AnyView handling deinit #68

--- MondrianLayout/AnyView.swift
+++ MondrianLayout/AnyView.swift
@@ -2,6 +2,8 @@ import UIKit
 
 open class AnyView: UIView {
 
+  private var _onDeinit: (() -> Void)?
+
   public init(build: (AnyView) -> LayoutContainer) {
     super.init(frame: .zero)
     mondrian.buildSubviews {
@@ -19,6 +21,14 @@ open class AnyView: UIView {
   @available(*, unavailable)
   public required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+
+  deinit {
+    _onDeinit?()
+  }
+
+  public func setOnDeinit(_ closure: @escaping () -> Void) {
+    _onDeinit = closure
   }
 
 }
