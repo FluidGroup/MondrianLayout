a2b37b4a37042a40be6644c6bf7e7baee07dc5c7
Muukii <muukii.app@gmail.com>
2022-09-02 03:50:47 +0900

    :evergreen_tree: Update

--- MondrianLayout/Descriptors/VHStackContentBuilder.swift
+++ MondrianLayout/Descriptors/VHStackContentBuilder.swift
@@ -231,6 +231,10 @@ public enum HStackContentBuilder {
   public static func buildEither(second component: [Component]) -> [Component] {
     return component
   }
+  
+  public static func buildArray(_ components: [[Component]]) -> [Component] {
+    components.flatMap { $0 }
+  }
 
   public static func buildExpression(_ views: [UIView]...) -> [Component] {
     return views.flatMap { $0 }.map {
